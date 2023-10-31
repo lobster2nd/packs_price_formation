358f5bc9eb4791bdae8d7d353f19c51a4ce138d2
LobsterX <lobster2@mail.ru>
2023-10-31 13:00:28 +0300

    fix: nullable fields in model now work correctly

--- api/v1/models.py
+++ api/v1/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class PurchasePlanningAnalysisData(models.Model):
-    date_from = models.DateField(blank=False)
-    date_to = models.DateField(blank=False)
-    our_warehouse = models.CharField(max_length=200, blank=False)
-    wb_warehouse = models.CharField(max_length=200, blank=True)
-    category = models.CharField(max_length=100, blank=True)
-    brand = models.CharField(max_length=100, blank=True)
-    product = models.IntegerField(blank=True)
-    cabinet = models.IntegerField(blank=True)
+    date_from = models.DateField(null=False)
+    date_to = models.DateField(null=False)
+    our_warehouse = models.CharField(max_length=200, null=False)
+    wb_warehouse = models.CharField(max_length=200, null=True)
+    category = models.CharField(max_length=100, null=True)
+    brand = models.CharField(max_length=100, null=True)
+    product = models.IntegerField(null=True)
+    cabinet = models.IntegerField(null=True)
